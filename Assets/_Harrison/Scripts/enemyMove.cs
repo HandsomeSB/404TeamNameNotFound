5a0212a7eb090ad35d15e172c31f04a85e8e5c3f
HandsomeSB <58240748+HandsomeSB@users.noreply.github.com>
2020-10-02 23:32:05 -0700

    Added tower

---
+++ Assets/_Harrison/Scripts/enemyMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct a
+{
+    public byte x, y, z;
+}
+
+public class owner
+{
+    private a v;
+    public a V
+    {
+        get { return v; }//returns a pointer to v
+        set { v = value; }
+    }
+}
+
+public class enemyMove : MonoBehaviour
+{
+    Vector3 velocity = new Vector3(-1f, 0f, 0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        owner MrV = new owner();//every variable of a class is a point to the "new" class, except for struct. Struct is passed by value not by pointer, a copy of the data. Struct is a little faster
+        //MrV.V.x += 1;
+        //MrV.V = new a(); Error
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += velocity * Time.deltaTime;
+        //transform.position.x += 1; Error because position is return as a value, x is a copy of the position, not the pointer to the actual position
+        GetComponent<Rigidbody>().velocity = velocity;
+    }
+}
---
+++ Assets/_Harrison/Scripts/raySelect.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raySelect : MonoBehaviour
+{
+
+    public GameObject marker;
+    Dictionary<KeyCode, System.Action> keyMap = new Dictionary<KeyCode, System.Action>();
+
+    public GameObject prefabMarker;
+    public GameObject prefabMarker1;
+
+    private System.Action thingsToDoLater;
+
+    public enum rayState { none, cube, ball};
+    public rayState towerState = rayState.none;
+
+    void stuff(){
+        Debug.Log("hi u suck");
+    }
+
+    void createRay(){
+      RaycastHit hit = new RaycastHit();
+      //LayerMask ignoreRay = LayerMask.GetMask("ignore Raycast");
+      Physics.Raycast(transform.position, transform.forward, out hit);//, Mathf.Infinity, ignoreRay);
+      if(marker){
+        marker.transform.position = hit.point;
+      }
+    }
+
+    void resetMarkerState()
+    {
+        towerState = rayState.none;
+        Destroy(marker);//Destroy the marker
+        thingsToDoLater = null;//Unassign the task
+        marker = null;//unassign the marker
+    }
+
+    void setMarkerState(rayState theState, GameObject theMarker)
+    {
+        if (towerState != rayState.none)
+        {
+            Destroy(marker);
+            marker = null;
+        }
+        towerState = theState;
+        thingsToDoLater = createRay;//assign the task
+        marker = Instantiate(theMarker);//assign the marker
+    }
+
+    void keyPressedForTowerState(rayState theState, GameObject theMarker)
+    {
+        if (towerState != theState)
+        {//if the task is unassgined or the task is not the same one
+            setMarkerState(theState, theMarker);
+
+        }
+        else
+        {
+            resetMarkerState();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      keyMap[KeyCode.E] = ()=>{
+          keyPressedForTowerState(rayState.cube, prefabMarker);
+      };
+      keyMap[KeyCode.R] = ()=> {
+          keyPressedForTowerState(rayState.ball, prefabMarker1);
+      };
+        keyMap[KeyCode.Mouse0] = () =>
+        {
+            if (marker)
+            {
+                GameObject copy = Instantiate(marker);
+                copy.GetComponent<rotator>().enabled = true;
+                //TODO create feature to upgrade towers
+                //TODO partical effects when new tower is created
+                //TODO play sounds when tower created
+                
+            }
+            
+        };
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      foreach(KeyValuePair<KeyCode,System.Action> entry in keyMap){
+        if(Input.GetKeyDown(entry.Key)){
+          entry.Value.Invoke();
+        }
+      }
+
+
+    }
+
+    void LateUpdate(){
+      if(thingsToDoLater != null){
+        thingsToDoLater.Invoke();
+        //thingsToDoLater = null;
+      }
+    }
+}
